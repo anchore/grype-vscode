b32b0cf1ee5852c130325f97a3d0b4fbba6505ca
Dan Luhring <dan.luhring@anchore.com>
2020-08-21 11:23:55 -0400

    Refactor
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export const config: IConfig = {
+    grype: {
+        repositoryURL: "github.com/anchore/grype",
+        requiredVersion: "0.1.0-beta.6",
+    },
+};
+
+export interface IConfig {
+    grype: IConfigGrype;
+}
+
+interface IConfigGrype {
+    repositoryURL: string;
+    requiredVersion: string;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 import * as vscode from 'vscode';
 import path = require("path");
 import fs = require("fs");
-import process = require("process");
-import {https} from 'follow-redirects';
-import tar = require("tar-stream");
-import zlib = require('zlib');
+import { config } from "./config";
+import { ExecutableHandler } from "./ExecutableHandler";
+import { determinePlatform } from './Platform';
 import StatusBar from "./status-bar";
+
 
 function createExtensionStorage(context: vscode.ExtensionContext): string {
 	if (context.globalStoragePath) {
@@ -22,73 +22,18 @@ function createExtensionStorage(context: vscode.ExtensionContext): string {
 	return context.globalStoragePath;
 }
 
-function storeGrypeApp(storagePath: string): string {
-	const grypeVersion = "0.1.0-beta.6";
-	let platform: string;
-	switch (process.platform) {
-		case "darwin":
-			platform = "darwin_amd64";
-			break;
-		case "linux":
-			platform = "linux_amd64";
-			break;
-
-		default:
-			platform = ""; // TODO: This should be a fatal error — how do we want to handle that?
-			break;
-	}
-
-	const exePath = path.resolve(storagePath, "grype");
-	if (fs.existsSync(exePath)) {
-		console.log("grype already exists locally")
-		return exePath;
-	}
-	console.log("downloading grype")
-
-	const archiveName = `grype_${grypeVersion}_${platform}.tar.gz`
-	const binaryURL = `https://github.com/anchore/grype/releases/download/v${grypeVersion}/${archiveName}`;
-
-	const request = https.get(binaryURL, response => {
-
-		var writeStream = fs.createWriteStream(exePath);
-		var extract = tar.extract();
-
-		extract.on('entry', (header, stream, next) => {
-
-			stream.on('data', (chunk) => {
-				if (header.name === 'grype') {
-					writeStream.write(chunk);
-				}
-			});
-
-			stream.on('end', () => {
-				next();
-			});
-
-			stream.resume();
-		});
-
-		extract.on('finish', () => {
-			writeStream.end();
-			fs.chmodSync(exePath, 0o755);
-		});
-
-		response
-			.pipe(zlib.createGunzip())
-			.pipe(extract);
-
-	});
-	request.on("error", e => console.error(e)); // TODO: What's the best way to handle this?
-
-	return exePath;
-}
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	// set the extension storage path (create if it does not exist)
 	const storagePath = createExtensionStorage(context);
-	const exePath = storeGrypeApp(storagePath);
+	const execHandler = new ExecutableHandler(config, storagePath, determinePlatform());
+
+  	execHandler.storeGrypeApp().then(() => {
+		console.log("grype extension activated");
+	}).catch(
+		console.error
+	);
 
 	const extension: GrypeExtension = new GrypeExtension(context);
 
