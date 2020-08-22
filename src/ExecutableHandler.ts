d54c8065447c0b0d63a89aebbca1d72184af0ae7
Dan Luhring <dan.luhring@anchore.com>
2020-08-21 21:30:49 -0400

    Apply prettier formatting
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>

--- src/ExecutableHandler.ts
+++ src/ExecutableHandler.ts
@@ -1,11 +1,11 @@
 import { IConfig } from "./config";
-import { https } from 'follow-redirects';
+import { https } from "follow-redirects";
 import tar = require("tar");
-import zlib = require('zlib');
-import { IncomingMessage } from 'http';
+import zlib = require("zlib");
+import { IncomingMessage } from "http";
 import path = require("path");
 import fs = require("fs");
-import { Platform } from './Platform';
+import { Platform } from "./Platform";
 
 export class ExecutableHandler {
   private config: IConfig;
@@ -34,7 +34,7 @@ export class ExecutableHandler {
 
   private async downloadGrype(): Promise<void> {
     return new Promise((resolve, reject) => {
-      const request = https.get(this.grypeArchiveURL(), async response => {
+      const request = https.get(this.grypeArchiveURL(), async (response) => {
         await this.handleGrypeArchiveStream(response);
         resolve();
       });
@@ -43,11 +43,18 @@ export class ExecutableHandler {
     });
   }
 
-  private async handleGrypeArchiveStream(response: IncomingMessage): Promise<void> {
-    return new Promise((resolve, _) => { // Consider listening for errors on all streams and rejecting the promise in response
+  private async handleGrypeArchiveStream(
+    response: IncomingMessage
+  ): Promise<void> {
+    return new Promise((resolve, _) => {
+      // Consider listening for errors on all streams and rejecting the promise in response
       response
         .pipe(zlib.createGunzip())
-        .pipe(tar.extract({ cwd: this.storagePath } as tar.ExtractOptions, ["grype"]))
+        .pipe(
+          tar.extract({ cwd: this.storagePath } as tar.ExtractOptions, [
+            "grype",
+          ])
+        )
         .on("close", resolve);
     });
   }
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,13 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import path = require("path");
 import fs = require("fs");
 import { config } from "./config";
 import { ExecutableHandler } from "./ExecutableHandler";
-import { determinePlatform } from './Platform';
+import { determinePlatform } from "./Platform";
 import GrypeExtension from "./GrypeExtension";
 
 function createExtensionStorage(context: vscode.ExtensionContext): string {
   if (context.globalStoragePath) {
-
     const root = path.resolve(context.globalStoragePath, "..");
     if (!fs.existsSync(root)) {
       fs.mkdirSync(root);
@@ -24,13 +23,18 @@ function createExtensionStorage(context: vscode.ExtensionContext): string {
 export function activate(context: vscode.ExtensionContext) {
   // set the extension storage path (create if it does not exist)
   const storagePath = createExtensionStorage(context);
-  const execHandler = new ExecutableHandler(config, storagePath, determinePlatform());
-
-	execHandler.storeGrypeApp().then(() => {
-    console.log("grype extension activated");
-  }).catch(
-    console.error
+  const execHandler = new ExecutableHandler(
+    config,
+    storagePath,
+    determinePlatform()
   );
+
+  execHandler
+    .storeGrypeApp()
+    .then(() => {
+      console.log("grype extension activated");
+    })
+    .catch(console.error);
 
   const extension = new GrypeExtension(context);
   extension.register();
@@ -39,12 +43,10 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-
 // handler (ensure we have the correct grype and it's downloaded) -> grype class
 
 class Grype {
-  constructor() {
-  }
+  constructor() {}
 
   public async version() {
     // invoke the grype exe and get the current version
@@ -57,8 +59,5 @@ class Grype {
     // calls exec
   }
 
-  private async exec(cmd: string) {
-  }
-
-
+  private async exec(cmd: string) {}
 }
